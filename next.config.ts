6eef628e3e2b6f1fc0cf8a9c4e4aae1742e9f5fd
abhishekChauhanx <rajputabhic12@gmail.com>
2026-06-10 16:48:13 +0530

    fix: add next-intl plugin to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,7 @@
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["@prisma/client", "prisma"],
@@ -6,4 +10,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withNextIntl(nextConfig);
