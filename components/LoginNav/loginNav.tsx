b4cfc76df57995d32c891e0b77e17ff0f4f31b63
abhishekChauhanx <rajputabhic12@gmail.com>
2026-06-11 21:27:24 +0530

    Add : SelectLang component inside the login Nav

--- components/LoginNav/loginNav.tsx
+++ components/LoginNav/loginNav.tsx
@@ -1,6 +1,7 @@
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import React from 'react'
 import { toggleTheme } from "@/store/themeSlice";
+import SelectLang from '../SelectLang/SelectLang';
 const loginNav = () => {
     const dispatch = useAppDispatch();
     const mode = useAppSelector((s) => s.theme.mode);
@@ -8,6 +9,10 @@ const loginNav = () => {
     return (
         <div className="lp-topbar">
             <span className="lp-logo">ImageFlow</span>
+             {/* Language Switcher */}
+
+
+        <SelectLang />
             <button
                 className="lp-theme-btn"
                 onClick={() => dispatch(toggleTheme())}
